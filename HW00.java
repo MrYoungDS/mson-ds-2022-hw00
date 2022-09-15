ffe16b27e06a89f8661043ca0a94202c9663e5f3
Steve Young <109768450+MrYoungDS@users.noreply.github.com>
2022-09-15 10:33:39 -0400

    Create new HW template
    HW0 A variation on Project Euler problem 1

---
+++ HW00.java
@@ -0,0 +1,28 @@
+/**
+ * MSON Data Structures 2022-23 Mr Young
+ * Homework 0
+ * A tiny assignment to see if Mr Young has github classroom working.
+ */
+public class HW00
+{
+  /**
+   * This is a variation on Project Euler problem 1.
+   * The input will be a single int n (but provided as a String in args[0]).
+   * The output should be the solution to this problem:
+   *
+   * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+   * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+   * Find the sum of all the multiples of 3 or 5 below n.
+   *
+   * Sample solutions
+   * Input: 10
+   * Output: 23
+   *
+   * Input: 100
+   * Output: 2318
+   */
+  public static void main(String args[])
+  {
+    
+  }
+}
